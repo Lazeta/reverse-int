b3d19365a4a92b50f1fc5f42b53bf17245c67b26
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-01-03 20:08:17 +0300

    feat: confirm task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    if (number >= 0) {
+        number = number + "";
+        return number.split("").reverse().join("");
+    } else {
+        number = -number + "";
+        return number.split("").reverse().join("");
+    }
 }
